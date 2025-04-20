29225c9ca5241821c682c829321c110d388a1d1f
Abdullah Mohsin <149489097+AbdullahMohsinU@users.noreply.github.com>
2025-04-20 15:20:11 +0500

    ANIMAL CLASSIFY SPECIES

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    // Hide preloader and display main content
+    document.getElementById('preloader').style.display = 'none';
+    document.getElementById('main-content').style.display = 'block';
+});
